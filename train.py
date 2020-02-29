17941cc69a3d2a9bd9aae6bb7f2f38380fe2f635
Satharuban <satharuban@gmail.com>
2020-02-29 20:21:01 +0530

    model creation

--- train.py
+++ train.py
@@ -1,22 +1,41 @@
 import pandas as pd
-
-df = pd.read_csv('iris.csv')
-
-
-x = df.values[0:,0]# feature
-
-y = df.values[0:,4:] #labels
+from keras.models import Sequential
 
 
-j = 0             #create a loop to for assign lables to numeric values
-for i in y:
-    if i=='setosa':
-        y[j,0]=0
-
-    elif i=='versicolor':
-        y[j,0]=1
-    else:
-        y[j,0]=2
-    j+=1
-
-
+# from keras.layers import Dense, Activation, Dropout
+#
+# df = pd.read_csv('iris.csv')
+#
+# x = df.values[0:, 0]  # feature
+#
+# y = df.values[0:, 4:]  # labels
+#
+# j = 0  # create a loop to for assign lables to numeric values
+# for i in y:
+#     if i == 'setosa':
+#         y[j, 0] = 0
+#
+#     elif i == 'versicolor':
+#         y[j, 0] = 1
+#     else:
+#         y[j, 0] = 2
+#     j += 1
+#
+# # nueral network creation
+# model = models.Sequential()
+#
+# model.add(Dense(32, input_shape=(4, 150)))
+# model.add(Activation('tanh'))
+# model.add(Dropout(0.5))
+#
+# model.add(Dense(32))
+# model.add(Activation('tanh'))
+# model.add(Dropout(0.5))
+#
+# model.add(Dense(3))
+# model.add(Activation('softmax'))
+# model.add(Dropout(0.5))
+#
+# model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+#
+# print(model.summary())
